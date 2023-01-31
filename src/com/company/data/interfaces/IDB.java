e46ee771a411dc2d35335586c9e07a31a61998c9
merfibae-boop <124030942+merfibae-boop@users.noreply.github.com>
2023-01-31 13:35:16 +0600

    Update IDB.java

--- src/com/company/data/interfaces/IDB.java
+++ src/com/company/data/interfaces/IDB.java
@@ -3,6 +3,6 @@ package com.company.data.interfaces;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-public interface IDB {
+public interface DDD {
     Connection getConnection() throws SQLException, ClassNotFoundException;
 }
